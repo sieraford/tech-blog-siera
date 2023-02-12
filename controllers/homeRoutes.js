ebdaa40f762e215455c0e5b7d7e91b2c7e2937db
Siera Ford <sieraford@Sieras-MacBook-Pro.local>
2023-02-12 15:56:20 -0800

    add logged_in to homeroute for homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -18,7 +18,10 @@ router.get('/', async (req, res) => {
     const posts = postData.map((post) => post.get({ plain: true }));
 
     // Pass serialized data and session flag into template
-    res.render('homepage', { posts });
+    res.render('homepage', { 
+      posts,
+      logged_in: req.session.logged_in 
+     });
   } catch (err) {
     res.status(500).json(err);
   }
